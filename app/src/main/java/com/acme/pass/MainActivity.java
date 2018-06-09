b6b95930b66819949b67735eb0a55b8741d5df71
Joshua Swain <Joshua.Swain@dal.ca>
2018-06-09 03:24:53 -0300

    Refactored by removing comments and shortening code in Validator.java

--- app/src/main/java/com/acme/pass/MainActivity.java
+++ app/src/main/java/com/acme/pass/MainActivity.java
@@ -14,30 +14,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // When R.id.validate is clicked,
         Button validateButton = findViewById(R.id.validateButton);
         validateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //  save the pw value as pw
+
                 EditText pw = findViewById(R.id.passwordInput);
                 TextView output = findViewById(R.id.strengthOutput);
                 Validator validator = new Validator();
                 int strength = 0;
 
-                // get it's strength from the validate method
                 try {
                     strength = validator.validate(pw.getText().toString());
                 }
                 catch (Exception e) { e.getStackTrace(); }
 
-                // if the strength is bigger than 3
                 if (strength > 3) {
-                    // set strengthOutput to "Strong enough"
                     output.setText(new String("Strong enough"));
                 }
                 else {
-                    // set strengthOutput to "Not strong enough"
                     output.setText(new String("Not strong enough"));
                 }
             }
--- app/src/main/java/com/acme/pass/Validator.java
+++ app/src/main/java/com/acme/pass/Validator.java
@@ -7,34 +7,27 @@ public class Validator {
     public int validate(String password) throws Exception {
         int passCount = 0;
 
-        // Test rule 1: cannot be a case-variant of "password"
-        String lowercasePassword = new String();
-        lowercasePassword = password.toLowerCase();
+        String lowercasePassword = password.toLowerCase();
         if (!(lowercasePassword.equals("password"))) {
             passCount++;
         }
 
-        // Test rule 2: must be at least 8 characters in length
         if (password.length() > 7) {
             passCount++;
         }
 
-        // Test rule 3: must not contain any spaces
         if (!password.matches(".*[ ].*")) {
             passCount++;
         }
 
-        // Test rule 4: must contain at least one digit
         if (password.matches(".*[0-9].*")) {
             passCount++;
         }
 
-        // Test rule 5: must be less than 33 characters
         if (password.length() < 33) {
             passCount++;
         }
 
-        // Test rule 6: must contain both upper-case and lower-case characters
         if (password.matches("(.*[a-z].*[A-Z].*)|(.*[A-Z].*[a-z].*)")) {
             passCount++;
         }
